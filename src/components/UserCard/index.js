b9e3183385334f71736b800e07e56c49e923dfcb
tanganmin <tanganmin@douyu.tv>
2017-03-31 16:59:46 +0800

    user-card

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,9 +1,53 @@
 import React, {Component, PropTypes} from 'react';
+import userIcon from './user-icon.jpg';
+import './user_card.css';
 
 export default class UserCard extends Component {
   render() {
     return (
-      <div class=""></div>
+      <div className="user-card">
+        <div className="card-bg">
+          <div className="head-pic">
+            <a href={this.props.userUrl}>
+              <img className="user-icon" src={userIcon} alt="头像"/>
+            </a>
+          </div>
+        </div>
+        <div className="user-info-wrap">
+          <div className="name-box">
+            <a className="user-name" href="#">{this.props.name}</a>
+            <a href="#">
+              <i className="level-icon"></i>
+            </a>
+            <a href="#">
+              <span className="level-box">
+                <span className="level-num">{this.props.levelNum}</span>
+              </span>
+            </a>
+          </div>
+          <ul className="user-atten">
+            <li>
+              <a href="">
+                <strong>7</strong>
+                <span>关注</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <strong>54</strong>
+                <span>粉丝</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <strong>463</strong>
+                <span>微博</span>
+              </a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
     );
   }
 }
@@ -12,12 +56,16 @@ UserCard.PropTypes = {
   name: React.PropTypes.string,
   focusNum: React.PropTypes.number,
   fansNum: React.PropTypes.number,
-  microBlogNum: React.PropTypes.number
+  microBlogNum: React.PropTypes.number,
+  userUrl: React.PropTypes.string,
+  levelNum: React.PropTypes.string
 }
 
 UserCard.defaultProps = {
   name: "哇哈哈",
   focusNum: 1,
   fansNum: 2,
-  microBlogNum: 3
+  microBlogNum: 3,
+  userUrl: 'http://www.baidu.com',
+  levelNum: 'Lv.9'
 }
--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -1,10 +1,12 @@
 import React, {Component, PropTypes} from 'react';
+import UserIcon from '../../components/UserCard';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         我这里是首页专区！！
+        <UserIcon/>
       </div>
     );
   }
